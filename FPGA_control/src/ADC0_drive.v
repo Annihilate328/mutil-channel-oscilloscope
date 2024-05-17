4a1c5f31de9573debd3dacd130ea7bf5efe2a095
lmz <mingzhelee328@gmail.com>
2024-05-18 03:49:46 +0800

    add new text code have some trouble

--- FPGA_control/src/ADC0_drive.v
+++ FPGA_control/src/ADC0_drive.v
@@ -1,13 +1,13 @@
 module ADC0_drive(
 	input Clk,
 	input Reset_n,
-	input [7:0] AD_Data,
-	input [7:0] Trigger,
+	input [9:0] AD_Data,
+	input [9:0] Trigger,
 	input ADC0_bg,
 	input rdreq,
 	output reg ADC0_end,
 	output empty,
-	output [7:0] ADC0_F_O_Data
+	output [9:0] ADC0_F_O_Data
 );
 	reg wrreq;
 	reg sclr;
@@ -37,14 +37,19 @@ module ADC0_drive(
 		end else begin
 			if (count <= 5130) begin
 				count <= count + 1;
-			end else if (count > 5129 && full == 1) begin
+				if (count >8 && count <1033 && Tri == 1) begin
+					count <= 13'd1033;
+				end
+			end else if (full == 1) begin
 				ADC0_end <= 1;
-			end else if (count > 5130 && ADC0_bg == 1) begin
+			end else if (ADC0_bg == 1 && empty == 1) begin
 				count <= 13'd0;
 				ADC0_end <= 0;
-			end else if (count > 5130 && empty == 1) begin
-				ADC0_end <= 0;
-			end
+			end 
+//			else if (count > 5130 && empty == 1) begin
+//				ADC0_end <= 0;
+//			end 
+			
 		end
 	end
 
@@ -58,7 +63,7 @@ module ADC0_drive(
 				state <= 3'd2;
 			else if(ADC0_end ==1)
 				state <= 3'd4;
-			else if (count > 1032 || Tri == 1)
+			else if (count > 1032 && count <5132)
 				state <= 3'd3;
 			else state <= 3'd0;
 		end
@@ -77,8 +82,8 @@ module ADC0_drive(
 				3'd2: begin
 					sclr <= 1'b0;
 					Tri <= 0;
-					if (AD_Data > Trigger) begin
-						Tri <= 1;
+					if (AD_Data > Trigger && AD_Data < (Trigger + 4'd15)) begin
+							Tri <= 1;
 					end
 				end
 				3'd3: begin
@@ -88,7 +93,7 @@ module ADC0_drive(
 					wrreq <= 1'b0;
 				end
 				default: begin
-					sclr <= 1'b0;
+					sclr <= 1'b1;
 					Tri <= 1'b0;
 					wrreq <= 1'b0;
 				end
--- FPGA_control/src/FPGA_control.v
+++ FPGA_control/src/FPGA_control.v
@@ -1,14 +1,14 @@
 module FPGA_control(
 	input Clk,
 	input Reset_n,
-	input wire [7:0] ADC0_Data,
+	input wire [9:0] ADC0_Data,
+	input ADC0_bg,
 //   input Trigger,
 	output wire ADC0_Clk,
-	output wire [7:0] ADC0_O_Data
+	output wire [9:0] ADC0_O_Data
 );
-
+//	reg ADC0_bg;
 	wire empty;
-	reg ADC0_bg;
 	wire ADC0_end;
 	reg ADC0_rdreq;
 	assign ADC0_Clk = Clk;
@@ -16,23 +16,30 @@ module FPGA_control(
 		.Clk(ADC0_Clk),
 		.Reset_n(Reset_n),
 		.AD_Data(ADC0_Data),
-		.Trigger(8'd50),
+		.Trigger(8'd512),
 		.ADC0_bg(ADC0_bg),
 		.ADC0_end(ADC0_end),
 		.rdreq(ADC0_rdreq),
 		.empty(empty),
 		.ADC0_F_O_Data(ADC0_O_Data)
 	);
-	always@(posedge Clk or negedge Reset_n)begin
-		if(!Reset_n)begin
-			ADC0_bg <= 0;
-		end else begin
-			if(ADC0_end == 1)
-				ADC0_bg <= 0;
-			else if (empty == 1)
-				ADC0_bg <= 1;
-		end
-	end
+//	always@(posedge Clk or negedge Reset_n)begin
+//		if(!Reset_n)begin
+//			ADC0_bg <= 1;
+//		end else begin
+//			if(ADC0_end == 1)
+//				ADC0_bg <= 0;
+//			else if (empty == 1 && text ==1024)
+//				ADC0_bg <= 1;
+//		end
+//	end
+//	reg [8:0] text;
+//	always@(posedge Clk)begin
+//		if(empty == 1 && text < 1024)
+//			text <= text +1'd1;
+//		else if(empty ==0)
+//			text <= 0;
+//	end
 	always@(posedge Clk or negedge Reset_n)begin
 		if(!Reset_n)begin
 		end else begin
